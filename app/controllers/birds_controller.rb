cd2618e9b989846b078d22be3836a44828c4592f
okorkov <alex.okorkov@gmail.com>
2021-02-17 10:33:16 -0800

    commit tree 337dc84e4f59849c55e3336d996450446dadcab2

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
-  end
-end
+    birds = Bird.all
+    render json: birds, except: [:created_at, :updated_at]
+  end 
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird.slice(:id, :name, :species)
+  end 
+end 
